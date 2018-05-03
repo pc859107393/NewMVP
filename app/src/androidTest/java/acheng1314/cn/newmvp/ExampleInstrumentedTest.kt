6d383e6dc6e8dd7f9be8a46e8ff2651415bdf565
cheng <859107393@qq.com>
2018-05-04 00:53:08 +0800

    acheng init project and library

---
+++ app/src/androidTest/java/acheng1314/cn/newmvp/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package acheng1314.cn.newmvp
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("acheng1314.cn.newmvp", appContext.packageName)
+    }
+}
