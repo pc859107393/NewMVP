f67afea65dbc6f5d9b19d945b212ffa7e3e49825
cheng <859107393@qq.com>
2018-05-06 16:57:13 +0800

    acheng update mvp and insert fragment presenter

---
+++ baselibrary/src/main/java/acheng1314/cn/baselibrary/mvp/presenter/FragmentPresenter.kt
@@ -0,0 +1,39 @@
+package acheng1314.cn.baselibrary.mvp.presenter
+
+import acheng1314.cn.baselibrary.mvp.view.VDelegate
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+abstract class FragmentPresenter<out T : VDelegate> : Fragment() {
+    private var vd: T? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        vd = initViewD()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return vd?.onCreateRootView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        vd?.initView()
+        vd?.initEvent()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        vd = null
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        vd = initViewD()
+    }
+
+    abstract fun initViewD(): T
+}
